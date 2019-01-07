3cee201760b5f33d741b3b857a88eea5736c2a85
Liewzheng <liewzheng@sina.com>
2019-01-07 13:52:45 +0800

    Initial commit

---
+++ build/bt/bluedroid/btc/core/btc_sec.d
@@ -0,0 +1,3 @@
+bluedroid/btc/core/btc_sec.o: \
+ C:/msys32/home/AtomTech/esp/esp-idf/components/bt/bluedroid/btc/core/btc_sec.c
+/home/AtomTech/esp/esp-idf/components/bt/bluedroid/btc/core/btc_sec.c:
---
+++ build/cxx/cxx_exception_stubs.d
@@ -0,0 +1,6 @@
+cxx_exception_stubs.o: \
+ C:/msys32/home/AtomTech/esp/esp-idf/components/cxx/cxx_exception_stubs.cpp \
+ D:/Eclipse-workspace/ble_ibeacon/build/include/sdkconfig.h
+
+D:/Eclipse-workspace/ble_ibeacon/build/include/sdkconfig.h:
+/home/AtomTech/esp/esp-idf/components/cxx/./cxx_exception_stubs.cpp:
